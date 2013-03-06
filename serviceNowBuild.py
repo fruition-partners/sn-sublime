ec949123292c2961b79dd7561b2086ac798db76c
Sal Costa <sal.costa@fruitionpartners.com>
2013-03-06 14:56:33 -0600

    Refactored code and added autocomplete and pull sync

--- serviceNowBuild.py
+++ serviceNowBuild.py
@@ -10,28 +10,45 @@ class ServiceNowBuildListener(sublime_plugin.EventListener):
     def on_pre_save(self, view):
         view.run_command('service_now_build')
 
+    def on_load(self,view):
+        sublime.set_timeout(syncFileCallback,10)
+        
+    def on_query_completions(self, view, prefix, locations):
+        real_prefix = ""
+        loc = locations[0];
+
+        if loc > 1:
+            back_loc = locations[0] - 1
+            space_found = False
+            while back_loc >= 1 and space_found == False:
+                back_loc = back_loc -1
+                reg = sublime.Region(back_loc, back_loc+1)
+                char = view.substr(reg)
+
+                if re.search(r"\s", char):
+                    back_loc = back_loc + 1
+                    space_found = True
+                if char == ".":
+                    loc = back_loc
+
+            reg = sublime.Region(back_loc, loc)
+            real_prefix = view.substr(reg).replace(".","")
+
+        if real_prefix == "gs":
+            completions = glide_system_completions
+                            
+            return completions
+
 
 class ServiceNowBuildCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        self.timeout = 5
-
         # Get the body of the file
         reg = sublime.Region(0, self.view.size())
         self.text = self.view.substr(reg)
 
-        # Get the file URL from the comment in the file
-        url_match = self.get_url(self.text)
-        if url_match:
-            url = url_match.groups()[0]
-        else:
-            print "Not a ServiceNow File"
-            return
-
-        # Get the instance name from the URL
-        instance = self.get_instance(url)
-
-        if not instance:
-            print "No instance found"
+        # Get the Base64 encoded Auth String
+        authentication = get_authentication(self, edit)
+        if not authentication:
             return
 
         # Get the field name from the comment in the file
@@ -39,65 +56,21 @@ class ServiceNowBuildCommand(sublime_plugin.TextCommand):
         
         if not fieldname:
             fieldname = "script"
-            print "No fieldname found, using default 'script'"
-            
-        # Get the Base64 encoded Auth String
-        authentication = self.get_authentication(edit, instance)
-
-        if authentication:
-            authentication = "Basic " + authentication
-        else:
-            print "No authentication found"
-            return
 
         try:
-            data = json.dumps({fieldname: self.text})
-            url = url + "&sysparm_action=update&JSON"
+            data = json.dumps({fieldname: self.text})            
+            url = self.url + "&sysparm_action=update&JSON"
             url = url.replace("sys_id", "sysparm_query=sys_id")
-            request = urllib2.Request(url, data)
-            request.add_header("Authorization", authentication)
-            request.add_header("Content-type", "application/json")
-            http_file = urllib2.urlopen(request, timeout=self.timeout)
-            self.result = http_file.read()
+            result = http_call(authentication, url, data)
             print "File Successully Uploaded"
             return
         except (urllib2.HTTPError) as (e):
             err = 'Error %s' % (str(e.code))
         except (urllib2.URLError) as (e):
             err = 'Error %s' % (str(e.code))
-
         print err
 
-    def get_authentication(self, edit, instance):
-        settings = sublime.load_settings('SN.sublime-settings')
-        reg = sublime.Region(0, self.view.size())
-        text = self.view.substr(reg)
-
-        authMatch = re.search(r"__authentication[\W=]*([a-zA-Z0-9:~`\!@#$%\^&*()_\-;,.]*)", text)
-
-        if authMatch and authMatch.groups()[0] != "STORED":
-            user_pass = authMatch.groups()[0]
-            authentication = self.store_authentication(edit, user_pass, instance)
-        else:
-            authentication = settings.get(instance)
-
-        if authentication:
-            return authentication
-        else:
-            return False
-
-    def store_authentication(self, edit, authentication, instance):
-        base64string = base64.encodestring(authentication).replace('\n', '')
-        reg = sublime.Region(0, self.view.size())
-        text = self.view.substr(reg)
-        self.text = text.replace(authentication, "STORED")
-        self.view.replace(edit, reg, self.text)
-
-        settings = sublime.load_settings('SN.sublime-settings')
-        settings.set(instance, base64string)
-        settings = sublime.save_settings('SN.sublime-settings')
-
-        return base64string
+        return
 
     def get_fieldname(self, text):
         fieldname_match = re.search(r"__fieldName[\W=]*([a-zA-Z0-9_]*)", text)
@@ -105,13 +78,146 @@ class ServiceNowBuildCommand(sublime_plugin.TextCommand):
             return fieldname_match.groups()[0]
         else:
             return False
+        
 
-    def get_url(self, text):
-        return re.search(r"__fileURL[\W=]*([a-zA-Z0-9:/.\-_?&=]*)", text)
+class ServiceNowSync(sublime_plugin.TextCommand):
+    def run(self, edit):
+        # Get the body of the file
+        reg = sublime.Region(0, self.view.size())
+        self.text = self.view.substr(reg)
 
-    def get_instance(self, url):
-        instance_match = re.search(r"//([a-zA-Z0-9]*)\.", url)
-        if instance_match:
-            return instance_match.groups()[0]
-        else:
-            return False
+        # Get the Base64 encoded Auth String
+        authentication = get_authentication(self, edit)
+        if not authentication:
+           return
+
+        try:
+            url = self.url + "&sysparm_action=get&JSON"
+            url = url.replace("sys_id", "sysparm_sys_id")
+            response_data = json.loads(http_call(authentication,url,{}))
+            serverText = response_data['records'][0]['script']
+
+            if self.text != serverText and sublime.ok_cancel_dialog("File has been updated on server. \nPress OK to Reload."):
+                self.view.erase(edit, reg)
+                self.view.begin_edit()
+                self.view.insert(edit,0,serverText)
+                self.view.end_edit(edit)
+            return
+        except (urllib2.HTTPError) as (e):
+            err = 'Error %s' % (str(e.code))
+        except (urllib2.URLError) as (e):
+            err = 'Error %s' % (str(e.code))
+        print err
+        
+
+def http_call(authentication, url, data):
+    timeout = 5
+    request = urllib2.Request(url, data)
+    request.add_header("Authorization", authentication)
+    request.add_header("Content-type", "application/json")
+    http_file = urllib2.urlopen(request, timeout=timeout)
+    result = http_file.read()
+
+    return result
+
+
+def get_authentication(sublimeClass, edit):
+    # Get the file URL from the comment in the file
+    sublimeClass.url = get_url(sublimeClass.text)
+    if not sublimeClass.url:
+        return False
+
+    # Get the instance name from the URL
+    instance = get_instance(sublimeClass.url)
+    if not instance:
+        return False
+
+    settings = sublime.load_settings('SN.sublime-settings')
+    reg = sublime.Region(0, sublimeClass.view.size())
+    text = sublimeClass.view.substr(reg)
+
+    authMatch = re.search(r"__authentication[\W=]*([a-zA-Z0-9:]*)", text)
+
+    if authMatch and authMatch.groups()[0] != "STORED":
+        user_pass = authMatch.groups()[0]
+        authentication = sublimeClass.store_authentication(edit, user_pass, instance)
+    else:
+        authentication = settings.get(instance)
+
+    if authentication:
+        return "Basic " + authentication
+    else:
+        print "No authentication found"        
+        return False
+
+
+def store_authentication(sublimeClass, edit, authentication, instance):
+    base64string = base64.encodestring(authentication).replace('\n', '')
+    reg = sublime.Region(0, sublimeClass.view.size())
+    text = sublimeClass.view.substr(reg)
+    sublimeClass.text = text.replace(authentication, "STORED")
+    sublimeClass.view.replace(edit, reg, sublimeClass.text)
+
+    settings = sublime.load_settings('SN.sublime-settings')
+    settings.set(instance, base64string)
+    settings = sublime.save_settings('SN.sublime-settings')
+
+    return base64string
+
+
+def get_url(text):
+    url_match = re.search(r"__fileURL[\W=]*([a-zA-Z0-9:/.\-_?&=]*)", text)
+
+    if url_match:
+        return url_match.groups()[0]
+    else:
+        print "Not a ServiceNow File"
+        return False
+
+
+def get_instance(url):
+    instance_match = re.search(r"//([a-zA-Z0-9]*)\.", url)
+    if instance_match:
+        return instance_match.groups()[0]
+    else:
+        print "No instance found"        
+        return False
+
+
+def syncFileCallback():
+    sublime.active_window().active_view().run_command('service_now_sync')
+
+glide_system_completions = ([("getUserID()", "getUserID()"),
+            ("dateTimeNow()", "dateTimeNow()"),
+            ("eventQueue()","eventQueue()"),
+            ("getDisplayColumn()","getDisplayColumn()"),
+            ("getEscapedProperty()","getEscapedProperty()"),
+            ("getMessage()","getMessage()"),
+            ("getProperty()","getProperty()"),
+            ("getMessage()","getMessage()"),
+            ("getProperty()","getProperty()"),
+            ("getStyle()","getStyle()"),
+            ("log()","log($0)"),
+            ("nil()","nil()"),
+            ("print()","print()"),
+            ("setProperty()","setProperty()"),
+            ("tableExists()","tableExists()"),
+            ("breaker()","breaker()"),
+            ("cacheFlush()","cacheFlush()"),
+            ("escaper()","escaper()"),
+            ("eventQueueScheduled()","eventQueueScheduled()"),
+            ("eventsProcess()","eventsProcess()"),
+            ("executeCondition()","executeCondition()"),
+            ("fileExists()","fileExists()"),
+            ("generateURL()","generateURL()"),
+            ("globalPut()","globalPut()"),
+            ("installationSetting()","installationSetting()"),
+            ("isPaused()","isPaused()"),
+            ("mergeAttribute()","mergeAttribute()"),
+            ("pause()","pause()"),
+            ("resolveIcon()","resolveIcon()"),
+            ("resume()","resume()"),
+            ("sleep()","sleep()"),
+            ("templateExists()","templateExists()"),
+            ("unWrap()","unWrap()")            
+            ])
